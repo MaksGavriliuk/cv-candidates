22e1dd16441a30ae015d04951fe06efa7036f668
MaksGavriliuk <maks.revival27@gmail.com>
2024-03-04 19:08:22 +0300

    create: entities Candidate and CandidateTest

---
+++ src/main/java/org/example/testtaskmaksimgavriliuk/entities/Candidate.java
@@ -0,0 +1,57 @@
+package org.example.testtaskmaksimgavriliuk.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.util.List;
+
+
+@Entity
+@Table(name = "candidates")
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Accessors(chain = true)
+public class Candidate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String surname;
+
+    private String name;
+
+    private String patronymic;
+
+    private String description;
+
+    @Lob
+    private byte[] photo;
+
+    @Lob
+    private byte[] cv;
+
+    @ManyToMany
+    @JoinTable(
+            name = "candidate_directions",
+            joinColumns = @JoinColumn(name = "candidate_id"),
+            inverseJoinColumns = @JoinColumn(name = "direction_id")
+    )
+    private List<Direction> directions;
+
+}
---
+++ src/main/java/org/example/testtaskmaksimgavriliuk/entities/CandidateTest.java
@@ -0,0 +1,47 @@
+package org.example.testtaskmaksimgavriliuk.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "candidate_tests")
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Accessors(chain = true)
+public class CandidateTest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "candidate_id")
+    private Candidate candidate;
+
+    @ManyToOne
+    @JoinColumn(name = "test_id")
+    private Test test;
+
+    private Integer score;
+
+    @Temporal(TemporalType.DATE)
+    private Date testDate;
+
+}
