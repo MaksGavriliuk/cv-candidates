627a6a4ed818ec119c2e8f096a8c12529a182ad3
MaksGavriliuk <maks.revival27@gmail.com>
2024-03-04 17:53:01 +0300

    create: entities Direction and Test

---
+++ src/main/java/org/example/testtaskmaksimgavriliuk/entities/Direction.java
@@ -0,0 +1,36 @@
+package org.example.testtaskmaksimgavriliuk.entities;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+
+@Entity
+@Table(name = "directions")
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Accessors(chain = true)
+public class Direction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+}
--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -2,14 +2,14 @@ CREATE TABLE directions
 (
     id          BIGSERIAL PRIMARY KEY,
     name        VARCHAR(255) NOT NULL,
-    description TEXT         NOT NULL
+    description TEXT
 );
 
 CREATE TABLE tests
 (
     id           BIGSERIAL PRIMARY KEY,
     name         VARCHAR(255) NOT NULL,
-    description  TEXT         NOT NULL,
+    description  TEXT,
     direction_id BIGINT       NOT NULL,
     FOREIGN KEY (direction_id) REFERENCES directions (id)
 );
