48c7a6eb2d6c26421852d7a6289406a5a46efee5
MaksGavriliuk <maks.revival27@gmail.com>
2024-03-04 18:38:11 +0300

    create: second migration file for candidates

---
+++ src/main/resources/db/migration/V2__candidates.sql
@@ -0,0 +1,30 @@
+CREATE TABLE candidates
+(
+    id          BIGSERIAL PRIMARY KEY,
+    surname     VARCHAR(255) NOT NULL,
+    name        VARCHAR(255) NOT NULL,
+    patronymic  VARCHAR(255) NOT NULL,
+    description TEXT         NOT NULL,
+    photo       BYTEA        NOT NULL,
+    cv          BYTEA        NOT NULL,
+);
+
+CREATE TABLE candidate_directions
+(
+    candidate_id BIGINT NOT NULL,
+    direction_id BIGINT NOT NULL,
+    PRIMARY KEY (candidate_id, direction_id),
+    FOREIGN KEY (candidate_id) REFERENCES candidates (id),
+    FOREIGN KEY (direction_id) REFERENCES directions (id)
+);
+
+CREATE TABLE candidate_tests
+(
+    id           BIGSERIAL PRIMARY KEY,
+    candidate_id BIGINT NOT NULL,
+    test_id      BIGINT NOT NULL,
+    score        BIGINT,
+    test_date    DATE,
+    FOREIGN KEY (candidate_id) REFERENCES candidates (id),
+    FOREIGN KEY (test_id) REFERENCES tests (id)
+);
