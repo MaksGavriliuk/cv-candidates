3e6d1f1c5bc38a2a0ee64232a3698860b879e497
MaksGavriliuk <maks.revival27@gmail.com>
2024-03-04 17:33:15 +0300

    git init

---
+++ src/main/java/org/example/testtaskmaksimgavriliuk/TestTaskMaksimGavriliukApplication.java
@@ -0,0 +1,13 @@
+package org.example.testtaskmaksimgavriliuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskMaksimGavriliukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskMaksimGavriliukApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/testtaskmaksimgavriliuk/TestTaskMaksimGavriliukApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.testtaskmaksimgavriliuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskMaksimGavriliukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
